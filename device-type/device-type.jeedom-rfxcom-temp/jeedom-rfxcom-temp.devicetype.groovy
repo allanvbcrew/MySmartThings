6799daf44b8f340a5aa3e46bc48fae5aa5698550
allanvbcrew <allan.sebastian.perso@gmail.com>
2015-10-27 12:04:56 +0000

    Update jeedom-rfxcom-temp.devicetype.groovy

--- device-type/device-type.jeedom-rfxcom-temp/jeedom-rfxcom-temp.devicetype.groovy
+++ device-type/device-type.jeedom-rfxcom-temp/jeedom-rfxcom-temp.devicetype.groovy
@@ -14,25 +14,51 @@ preferences {
 
 // for the UI
 metadata {
-	definition (name: "Jeedom RFXCom Temp", namespace: "smartthings-users", author: "dianoga7@3dgo.net") {
-		capability "Polling"
+	definition (name: "Jeedom RFXCom Temp", namespace: "MySmartThings", author: "allanvbcrew") {
 		capability "Relative Humidity Measurement"
-		capability "Thermostat"
 		capability "Temperature Measurement"
-		capability "Presence Sensor"
-		capability "Sensor"
-
-		command "away"
-		command "present"
-		command "setPresence"
-		command "heatingSetpointUp"
-		command "heatingSetpointDown"
-		command "coolingSetpointUp"
-		command "coolingSetpointDown"
-		command "setFahrenheit"
-		command "setCelsius"
-
-		attribute "temperatureUnit", "string"
+		capability "Battery"
 	}
+	
+	simulator {
+		// TODO: define status and reply messages here
+	}
+	
+	tiles {
+		valueTile("temperature", "device.temperature", canChangeIcon: true, canChangeBackground:true) {
+			state("temperature", label: '${currentValue}°', backgroundColors: [
+				// Celsius Color Range
+				[value: 0, color: "#153591"],
+				[value: 7, color: "#1e9cbb"],
+				[value: 15, color: "#90d2a7"],
+				[value: 23, color: "#44b621"],
+				[value: 29, color: "#f1d801"],
+				[value: 33, color: "#d04e00"],
+				[value: 36, color: "#bc2323"],
+			]
+			)
+		}
+		
+		valueTile("humidity", "device.humidity", inactiveLabel: false) {
+			state "default", label:'${currentValue}%', unit:"Humidity"
+		}
+		
+		valueTile("battery", "device.battery", decoration: "flat", inactiveLabel: false, width: 2, height: 2) {
+        		state "battery", label:'${currentValue}% battery', unit:""
+    		}
+    		
+    		main(["temperature", "humidity"])
+    		details(["temperature","humidity","battery"])
+	}
+}
 
+//handle commands
+def temperature = new physicalgraph.device.HubAction(
+    method: "GET",
+    path: "/jeedom/core/api/jeeApi.php",
+    headers: [
+        HOST: "192.168.0.20"
+    ],
+    query: [apikey: "xdie3axifu43nk9jqn07", type: "cmd", id: "11"]
+)
 
